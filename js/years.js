95d76f4dcc77289bb1231ca0d56708df5cc8799b
Gonzalez <luciano.gonzalez01@sap.com>
2018-04-12 12:27:08 -0300

    Refactor years

--- js/years.js
+++ js/years.js
@@ -1,11 +1,17 @@
 //years.js: scripts to handle year events
 
 $('.year').hover(function(){
-	var year = $(this);
-	year.switchClass('whiteFill', 'violet', 1000, 'easeInOutQuad');
-	year.next().switchClass('violet', 'whiteFill', 1000);
+	var yearGroup = $(this);
+	var yearPath = yearGroup.children("path");
+	var yearText = yearGroup.children("text");
+
+	yearPath.switchClass('whiteFill', 'violet', 1000, 'easeInOutQuad');
+	yearText.switchClass('violet', 'whiteFill', 1000);
 }, function(){
-	var year = $(this);
-	year.switchClass('violet', 'whiteFill', 1000, 'easeInOutQuad');
-	year.next().switchClass('whiteFill', 'violet', 1000, 'easeInOutQuad');
+	var yearGroup = $(this);
+	var yearPath = yearGroup.children("path");
+	var yearText = yearGroup.children("text");
+
+	yearPath.switchClass('violet', 'whiteFill', 1000, 'easeInOutQuad');
+	yearText.switchClass('whiteFill', 'violet', 1000, 'easeInOutQuad');
 });
